3aae46d8207592723cb5562660a7608edc3f392f
imransihab0 <imran.sihab.0@gmail.com>
2024-04-19 17:20:58 +0600

    adding notice page

---
+++ lib/features/noticeBoard.dart
@@ -0,0 +1,58 @@
+import 'dart:developer';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class noticeBoardPage extends StatefulWidget {
+  const noticeBoardPage({Key? key}) : super(key: key);
+
+  @override
+  State<noticeBoardPage> createState() => _noticeBoardPageState();
+}
+
+WebViewController controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..setBackgroundColor(const Color(0x00000000))
+  ..setNavigationDelegate(
+    NavigationDelegate(
+      onProgress: (int progress) {
+        // Update loading bar.
+      },
+      onPageStarted: (String url) {},
+      onPageFinished: (String url) {},
+      onWebResourceError: (WebResourceError error) {},
+      onNavigationRequest: (NavigationRequest request) {
+        if (request.url.startsWith('https://softengen.com/')) {
+          return NavigationDecision.prevent;
+        } else if (request.url.toLowerCase().endsWith('.pdf')) {
+          log('PDF link encountered: ${request.url}');
+          _launchPDF(request.url);
+          return NavigationDecision.prevent;
+        }
+        return NavigationDecision.navigate;
+      },
+    ),
+  )
+  ..loadRequest(Uri.parse('https://daffodilvarsity.edu.bd/noticeboard/'));
+
+Future<void> _launchPDF(String url) async {
+  try {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  } catch (e) {
+    log('Error launching PDF: $e');
+  }
+}
+
+class _noticeBoardPageState extends State<noticeBoardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
