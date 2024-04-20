0b57a8bd60d7c423c58f31f451170af91a0b9f4f
imransihab0 <imran.sihab.0@gmail.com>
2024-04-20 08:47:59 +0600

    adding blc page

--- lib/core/constants&variables/constants.dart
+++ lib/core/constants&variables/constants.dart
@@ -1,3 +1,4 @@
 const routine_api = "https://diuroutineapi-production.up.railway.app";
 
 const routine_api_base = "diuroutineapi-production.up.railway.app";
+
--- lib/features/blc/presentation/pages/blc_main.dart
+++ lib/features/blc/presentation/pages/blc_main.dart
@@ -1,10 +1,58 @@
+import 'dart:developer';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class BLCPage extends StatelessWidget {
-  const BLCPage({super.key});
+class blcPage extends StatefulWidget {
+  const blcPage({Key? key}) : super(key: key);
 
   @override
+  State<blcPage> createState() => _blcPageState();
+}
+
+WebViewController controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..setBackgroundColor(const Color(0x00000000))
+  ..setNavigationDelegate(
+    NavigationDelegate(
+      onProgress: (int progress) {
+        // Update loading bar.
+      },
+      onPageStarted: (String url) {},
+      onPageFinished: (String url) {},
+      onWebResourceError: (WebResourceError error) {},
+      onNavigationRequest: (NavigationRequest request) {
+        if (request.url.startsWith('https://softengen.com/')) {
+          return NavigationDecision.prevent;
+        } else if (request.url.toLowerCase().endsWith('.pdf')) {
+          log('PDF link encountered: ${request.url}');
+          _launchPDF(request.url);
+          return NavigationDecision.prevent;
+        }
+        return NavigationDecision.navigate;
+      },
+    ),
+  )
+  ..loadRequest(Uri.parse('https://elearn.daffodilvarsity.edu.bd/'));
+
+Future<void> _launchPDF(String url) async {
+  try {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  } catch (e) {
+    log('Error launching PDF: $e');
+  }
+}
+
+class _blcPageState extends State<blcPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: WebViewWidget(controller: controller),
+    );
   }
 }
