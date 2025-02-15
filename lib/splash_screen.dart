f5006bf38a77f0598c46c8a5450db5974cd22adb
Muntasir <md.muntasir2700@gmail.com>
2025-02-15 09:42:31 +0600

    Applying clean architecture

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,25 +1,21 @@
 import 'dart:async';
-import 'package:connectivity_plus/connectivity_plus.dart';
+
 import 'package:device_info_plus/device_info_plus.dart';
-import 'package:diu_student/core/common/app%20user/userCubit/app_user_cubit.dart';
+
 import 'package:diu_student/features/home/presentation/pages/homePage.dart';
-import 'package:diu_student/features/result%20analysis/presentation/pages/result_page.dart';
-import 'package:diu_student/features/routine/presentation/pages/routine_page.dart';
-import 'package:diu_student/features/web%20services/pages/noticeBoard.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:lottie/lottie.dart';
-import 'core/remote info/get_main_kamla_info.dart';
+import 'core/remote info/get_info.dart';
 import 'core/resources/information_repository.dart';
 import 'features/authentication/presentation/pages/login.dart';
 import 'features/authentication/presentation/widgets/textStyle.dart';
-import 'features/home/data/data_sources/local/local_routine.dart';
-import 'features/home/data/data_sources/local/local_user_info.dart';
 import 'firebase_options.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -63,18 +59,18 @@ class _SplashScreenState extends State<SplashScreen> {
       Map _info = _box.get("UserInfo");
 
       await getApiLink(); // Updates API link from backend
-      await getUserInfo(); // Gets User information
+      // await getUserInfo(); // Gets User information
 
       //sets user
       UserRole = _info["user"];
 
       // Gets routine according user information
-      if (_info["user"] == "Student") {
-        await getRoutineLocally(
-            _info["department"], "${_info["batch"]}${_info["section"]}", true);
-      } else {
-        await getRoutineLocally(_info["department"], _info["ti"], false);
-      }
+      // if (_info["user"] == "Student") {
+      //   await getRoutineLocally(
+      //       _info["department"], "${_info["batch"]}${_info["section"]}", true);
+      // } else {
+      //   await getRoutineLocally(_info["department"], _info["ti"], false);
+      // }
     }
 
     hasUser
