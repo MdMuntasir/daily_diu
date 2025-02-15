869a66df4ee88082dcee03d2ef97011bbbbecbd9
Muntasir <md.muntasir2700@gmail.com>
2025-02-15 15:17:24 +0600

    Applying clean architecture

--- lib/core/util/widgets/showRoutine.dart
+++ lib/core/util/widgets/showRoutine.dart
@@ -2,10 +2,7 @@ import 'dart:async';
 import 'package:diu_student/core/util/Entities/slot.dart';
 import 'package:diu_student/core/util/widgets/dayButton.dart';
 import 'package:diu_student/core/util/widgets/slotShower.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../../resources/information_repository.dart';
-import '../model/slot.dart';
 
 class ShowRoutine extends StatefulWidget {
   final double height;
@@ -18,200 +15,181 @@ class ShowRoutine extends StatefulWidget {
 }
 
 class _ShowRoutineState extends State<ShowRoutine> {
-  List<String> days = [
+  static const List<String> days = [
+    "Saturday",
+    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday",
-    "Sunday"
   ];
-  String choosed_day = "Saturday";
-  DateTime _dayTime = DateTime.now();
-  int _toDay = DateTime.now().weekday - 1;
-  int dayIndex = DateTime.now().weekday - 1;
-  bool start = true;
-  Timer? timer;
+
+  final weekdayToIndex = {
+    1: 2,
+    2: 3,
+    3: 4,
+    4: 5,
+    5: 6,
+    6: 0,
+    7: 1,
+  };
+
+  late String choosed_day;
+  late DateTime _dayTime;
+  late final Map<String, List<SlotEntity>> _sortedSlots;
+  late final Map<String, List<Widget>> _cachedSlotCards;
+
+  Timer? _timer;
 
   @override
   void initState() {
     super.initState();
-    timer = Timer.periodic(Duration(milliseconds: 10), (_) {
-      if (_dayTime.minute != DateTime.now().minute) {
-        _dayTime = DateTime.now();
-        setState(() {});
+    _dayTime = DateTime.now();
+
+    choosed_day = days[weekdayToIndex[_dayTime.weekday]!];
+
+    _sortedSlots = _sortSlotsByDay();
+    _cachedSlotCards = {};
+
+    _timer = Timer.periodic(const Duration(minutes: 1), (_) {
+      final now = DateTime.now();
+      if (_dayTime.minute != now.minute) {
+        setState(() => _dayTime = now);
       }
     });
   }
 
   @override
   void dispose() {
-    timer?.cancel();
+    _timer?.cancel();
     super.dispose();
   }
 
-  void _sat() {
-    choosed_day = 'Saturday';
-    setState(() {});
+  Map<String, List<SlotEntity>> _sortSlotsByDay() {
+    final Map<String, List<SlotEntity>> sorted = {};
+    for (final day in days) {
+      sorted[day] = widget.slots.where((slot) => slot.day == day).toList();
+    }
+    return sorted;
   }
 
-  void _sun() {
-    choosed_day = 'Sunday';
-    setState(() {});
+  List<Widget> _buildSlotCards(String day) {
+    // Return cached cards if available
+    if (_cachedSlotCards.containsKey(day)) {
+      return _cachedSlotCards[day]!;
+    }
+
+    final slots = _sortedSlots[day] ?? [];
+    final List<Widget> cards = [];
+    final double h = MediaQuery.of(context).size.height;
+    final time = _dayTime.hour + _dayTime.minute / 60;
+
+    for (final slot in slots) {
+      final List<String> splited = slot.time!.toString().split("-");
+      final timeRange = _parseTimeRange(splited);
+
+      final bool classNow = time >= timeRange[0] &&
+          time < timeRange[1] &&
+          days[weekdayToIndex[_dayTime.weekday]!] == day;
+
+      cards.add(SlotShower(
+        slots: slot,
+        isActive: classNow,
+      ));
+      cards.add(SizedBox(height: h * .03));
+    }
+
+    // Cache the cards
+    _cachedSlotCards[day] = cards;
+    return cards;
   }
 
-  void _mon() {
-    choosed_day = 'Monday';
-    setState(() {});
+  List<double> _parseTimeRange(List<String> timeRange) {
+    List<double> result = [];
+    for (String time in timeRange) {
+      final parts = time.split(":");
+      double hours = double.parse(parts[0]);
+      hours = hours < 7 ? hours + 12 : hours;
+      final minutes = double.parse(parts[1]);
+      result.add(hours + minutes / 60);
+    }
+    return result;
   }
 
-  void _tue() {
-    choosed_day = 'Tuesday';
-    setState(() {});
-  }
+  Widget _buildDayButtons() {
+    final Map<String, List<dynamic>> dayShort = {
+      "Saturday": ["Sat", () => setState(() => choosed_day = "Saturday")],
+      "Sunday": ["Sun", () => setState(() => choosed_day = "Sunday")],
+      "Monday": ["Mon", () => setState(() => choosed_day = "Monday")],
+      "Tuesday": ["Tue", () => setState(() => choosed_day = "Tuesday")],
+      "Wednesday": ["Wed", () => setState(() => choosed_day = "Wednesday")],
+      "Thursday": ["Thu", () => setState(() => choosed_day = "Thursday")],
+    };
 
-  void _wed() {
-    choosed_day = 'Wednesday';
-    setState(() {});
-  }
-
-  void _thu() {
-    choosed_day = 'Thursday';
-    setState(() {});
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: days
+          .where((day) => day != "Friday")
+          .map((day) => DayButton(
+                function: dayShort[day]![1],
+                fullDay: day,
+                shortDay: dayShort[day]![0],
+                current: choosed_day == day,
+              ))
+          .toList(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    double h = MediaQuery.of(context).size.height;
-    double w = MediaQuery.of(context).size.width;
+    final double h = MediaQuery.of(context).size.height;
+    final double w = MediaQuery.of(context).size.width;
+    final bool horizontal = h > w;
+    final double effectiveHeight = widget.height == 0 ? 45 : widget.height;
+    final List<Widget> slotCards = _buildSlotCards(choosed_day);
 
-    double height = widget.height == 0 ? 45 : widget.height;
-
-    bool horizontal = h > w;
-
-    if (start) {
-      choosed_day = choosed_day != days[_dayTime.weekday - 1]
-          ? days[_dayTime.weekday - 1]
-          : choosed_day;
-      start = false;
-    }
-
-    List<SlotEntity> TodaySlots = [];
-
-    widget.slots.forEach((slot) {
-      if (slot.day == choosed_day) {
-        TodaySlots.add(slot);
-      }
-    });
-
-    // Show routine slots of a day
-    List<Widget> SlotCards = [];
-
-    TodaySlots.forEach((slot) {
-      List splited = slot.time!.toString().split("-");
-
-      List start = [
-            int.parse(splited[0].split(":")[0]),
-            int.parse(splited[0].split(":")[1])
-          ],
-          end = [
-            int.parse(splited[1].split(":")[0]),
-            int.parse(splited[1].split(":")[1])
-          ];
-
-      start[0] = start[0] < 7 ? start[0] + 12 : start[0];
-      end[0] = end[0] < 7 ? end[0] + 12 : end[0];
-
-      splited[0] = "${start[0] + start[1] / 60}";
-      splited[1] = "${end[0] + end[1] / 60}";
-
-      double time = _dayTime.hour + _dayTime.minute / 60;
-      bool classNow =
-          time >= double.parse(splited[0]) && time < double.parse(splited[1]);
-
-      SlotCards.add(SlotShower(
-        slots: slot,
-        isActive: classNow && days[_toDay] == choosed_day,
-      ));
-      SlotCards.add(SizedBox(
-        height: h * .03,
-      ));
-    });
-
-    //Buttons to shift days
-    List<Widget> dayButtons = [];
-
-    Map day_short = {
-      "Saturday": ["Sat", _sat],
-      "Sunday": ["Sun", _sun],
-      "Monday": ["Mon", _mon],
-      "Tuesday": ["Tue", _tue],
-      "Wednesday": ["Wed", _wed],
-      "Thursday": ["Thu", _thu],
-    };
-
-    Days.forEach(((day) {
-      if (day != "Friday") {
-        dayButtons.add(DayButton(
-          function: day_short[day][1],
-          fullDay: day,
-          shortDay: day_short[day][0],
-          current: choosed_day == day,
-        ));
-      }
-    }));
-
-    return Container(
-      child: Column(
-        children: [
-          SizedBox(
-            height: horizontal ? h * .1 : w * .1,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: dayButtons,
-            ),
-          ),
-          SizedBox(
-            height: horizontal ? h * .01 : w * .01,
-          ),
-          SlotCards.isEmpty
-              ? SizedBox(
-                  height: h * .01 * height,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    spacing: h * .03,
-                    children: [
-                      SizedBox(
-                        height: h * .2,
-                        child: Image.asset("assets/images/Sleeping.png"),
-                      ),
-                      Text(
-                        days[_toDay] == choosed_day
-                            ? "No class today\nEnjoy your day"
-                            : "This is your off day",
-                        style: const TextStyle(
-                          fontFamily: "Madimi",
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.deepPurple,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                )
-              : SizedBox(
-                  height: h * .01 * height,
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: SlotCards,
-                    ),
-                  ),
+    return Column(
+      children: [
+        SizedBox(
+          height: horizontal ? h * .1 : w * .1,
+          child: _buildDayButtons(),
+        ),
+        SizedBox(height: horizontal ? h * .01 : w * .01),
+        SizedBox(
+          height: h * .01 * effectiveHeight,
+          child: slotCards.isEmpty
+              ? _buildEmptyDayView(h)
+              : SingleChildScrollView(
+                  child: Column(children: slotCards),
                 ),
-        ],
-      ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildEmptyDayView(double h) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: h * .2,
+          child: Image.asset("assets/images/Sleeping.png"),
+        ),
+        Text(
+          days[weekdayToIndex[_dayTime.weekday]!] == choosed_day
+              ? "No class today\nEnjoy your day"
+              : "This is your off day",
+          style: const TextStyle(
+            fontFamily: "Madimi",
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.deepPurple,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
     );
   }
 }
