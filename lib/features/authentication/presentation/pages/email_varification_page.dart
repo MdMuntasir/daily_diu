9ac90f61cec18f04af4d49679789e2ea94bf814c
Muntasir <md.muntasir2700@gmail.com>
2025-02-19 21:04:30 +0600

    Applying clean architecture

--- lib/features/authentication/presentation/pages/email_varification_page.dart
+++ lib/features/authentication/presentation/pages/email_varification_page.dart
@@ -1,23 +1,25 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:diu_student/core/util/Entities/user_info.dart';
 import 'package:diu_student/core/util/widgets/show_message.dart';
+import 'package:diu_student/features/authentication/presentation/bloc/auth_bloc.dart';
+import 'package:diu_student/features/authentication/presentation/bloc/auth_event.dart';
+import 'package:diu_student/features/authentication/presentation/bloc/auth_state.dart';
 import 'package:diu_student/features/authentication/presentation/pages/signup_page.dart';
+import 'package:diu_student/main.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../../../../core/resources/information_repository.dart';
-import '../../../home/data/data_sources/local/local_routine.dart';
-import '../../../home/data/data_sources/local/local_user_info.dart';
-import '../../../../core/util/model/user_info.dart';
-import '../../../home/data/repository/user_info_store.dart';
-import '../../../home/presentation/pages/homePage.dart';
 
 class EmailVerifyScreen extends StatefulWidget {
-  final bool isStudent;
-  final String docId;
+  final UserEntity user;
 
-  EmailVerifyScreen({super.key, required this.isStudent, required this.docId});
+  const EmailVerifyScreen({
+    super.key,
+    required this.user,
+  });
 
   @override
   State<EmailVerifyScreen> createState() => _EmailVerifyScreenState();
@@ -25,96 +27,40 @@ class EmailVerifyScreen extends StatefulWidget {
 
 class _EmailVerifyScreenState extends State<EmailVerifyScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  bool Initialized = false;
+  bool initialized = false;
   bool verified = false;
+  late Timer _timer;
 
   @override
   void initState() {
-    Timer _timer = Timer.periodic(Duration(seconds: 2), (timer) async {
-      if (!Initialized) {
-        widget.isStudent
-            ? await FirebaseFirestore.instance
-                .collection("student")
-                .doc(widget.docId)
-                .update({'docID': widget.docId})
-            : await FirebaseFirestore.instance
-                .collection("teacher")
-                .doc(widget.docId)
-                .update({'docID': widget.docId});
+    Bloc bloc = context.read<AuthBloc>();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) async {
+      if (!initialized) {
+        await FirebaseFirestore.instance
+            .collection(widget.user.user == "Student" ? "student" : "teacher")
+            .doc(widget.user.docID)
+            .update({'docID': widget.user.docID});
+
+        initialized = true;
       }
 
       _auth.currentUser?.reload();
       if (_auth.currentUser!.emailVerified) {
         timer.cancel();
-
-        setState(() {
-          verified = true;
-        });
-
-        FirebaseFirestore _db = FirebaseFirestore.instance;
-        User _user = _auth.currentUser!;
-
-        widget.isStudent
-            ? await FirebaseFirestore.instance
-                .collection("student")
-                .doc(widget.docId)
-                .update({'verified': true}).then(
-                (value) async {
-                  final snapshot = await _db
-                      .collection("student")
-                      .where("email", isEqualTo: _user.email)
-                      .get();
-                  StudentInfoModel userData = snapshot.docs
-                      .map((e) => StudentInfoModel.fromSnapshot(e))
-                      .single;
-
-                  await getRoutineLocally(userData.department,
-                          "${userData.batch}${userData.section}", true)
-                      .then((_) async {
-                    StoreUserInfo(userData, true);
-                    await getUserInfo();
-
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        snackBarAnimationStyle:
-                            AnimationStyle(duration: Duration(seconds: 2)),
-                        SnackBar(content: Text("Successfully Signed Up")));
-
-                    UserRole = "Student";
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) => homePage()));
-                  });
-                },
-              )
-            : await FirebaseFirestore.instance
-                .collection("teacher")
-                .doc(widget.docId)
-                .update({'verified': true}).then((value) async {
-                final snapshot = await _db
-                    .collection("teacher")
-                    .where("email", isEqualTo: _user.email)
-                    .get();
-                TeacherInfoModel userData = snapshot.docs
-                    .map((e) => TeacherInfoModel.fromSnapshot(e))
-                    .single;
-
-                await getRoutineLocally(userData.department, userData.ti, false)
-                    .then((_) async {
-                  StoreUserInfo(userData, false);
-                  await getUserInfo();
-
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      snackBarAnimationStyle:
-                          AnimationStyle(duration: Duration(seconds: 2)),
-                      SnackBar(content: Text("Successfully Signed Up")));
-
-                  UserRole = "Teacher";
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => homePage()));
-                });
-              });
+        if (context.mounted) {
+          bloc.add(AuthVerifyEvent(
+            user: widget.user,
+          ));
+        }
       }
     });
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
   }
 
   @override
@@ -125,123 +71,116 @@ class _EmailVerifyScreenState extends State<EmailVerifyScreen> {
     void _resendMail() async {
       await _auth.currentUser?.sendEmailVerification().then(
         (value) {
-          showDialog(
-              context: context,
-              builder: (context) =>
-                  ShowAlertMessage(text: "Email has been Sent"));
+          if (context.mounted) {
+            showDialog(
+                context: context,
+                builder: (context) =>
+                    const ShowAlertMessage(text: "Email has been Sent"));
+          }
         },
       );
     }
 
     void _goBack() async {
-      widget.isStudent
-          ? await _auth.currentUser?.delete().then(
-              (value) async {
-                await FirebaseFirestore.instance
-                    .collection("student")
-                    .doc(widget.docId)
-                    .delete()
-                    .then(
-                      (value) => Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SignupPage())),
-                    );
-              },
-            )
-          : await _auth.currentUser?.delete().then(
-              (value) async {
-                await FirebaseFirestore.instance
-                    .collection("teacher")
-                    .doc(widget.docId)
-                    .delete()
-                    .then(
-                      (value) => Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SignupPage())),
-                    );
-              },
-            );
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => SignupPage()));
     }
 
-    return Scaffold(
-      body: Container(
-        margin: EdgeInsets.symmetric(vertical: 10, horizontal: w * .1),
-        height: h,
-        width: w,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              FontAwesomeIcons.envelope,
-              size: 120,
-              color: Colors.red.shade300,
-            ),
-            SizedBox(
-              height: h * .05,
-            ),
-            const Text(
-              "Verify Email",
-              style: TextStyle(
-                  fontSize: 38,
-                  fontFamily: "Times New Roman",
-                  fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: h * .02,
-            ),
-            const Text(
-              "A verification email has been sent to your email address. Please follow the link attached with the mail to verify your email.",
-              style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: "Times New Roman",
-                  fontWeight: FontWeight.w400),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: h * .02,
-            ),
-            TextButton(
-                onPressed: _goBack,
-                child: SizedBox(
-                  width: w * .5,
-                  height: h * .05,
-                  child: Center(
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.arrow_back,
-                        color: Colors.blue,
-                      ),
-                      title: Text(
-                        "Signup Page",
-                        style: TextStyle(color: Colors.blue),
-                      ),
-                    ),
+    return BlocConsumer(
+        bloc: context.read<AuthBloc>(),
+        buildWhen: (previous, current) => current is AuthVerification,
+        listener: (context, state) {
+          if (state is AuthVerifiedState) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text(state.message)));
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => const MyApp()));
+          } else if (state is AuthVerificationFailed) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text(state.message)));
+          }
+        },
+        builder: (context, state) {
+          return Scaffold(
+            body: Container(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: w * .1),
+              height: h,
+              width: w,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    FontAwesomeIcons.envelope,
+                    size: 120,
+                    color: Colors.red.shade300,
                   ),
-                )),
-            verified
-                ? CupertinoActivityIndicator()
-                : TextButton(
-                    onPressed: _resendMail,
-                    child: SizedBox(
-                      width: w * .5,
-                      height: h * .04,
-                      child: const ListTile(
-                        leading: Icon(
-                          Icons.refresh,
-                          color: Colors.green,
-                        ),
-                        title: Text(
-                          "Resend mail",
-                          style: TextStyle(color: Colors.green),
-                        ),
-                      ),
-                    )),
-          ],
-        ),
-      ),
-    );
+                  SizedBox(
+                    height: h * .05,
+                  ),
+                  const Text(
+                    "Verify Email",
+                    style: TextStyle(
+                        fontSize: 38,
+                        fontFamily: "Times New Roman",
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: h * .02,
+                  ),
+                  const Text(
+                    "A verification email has been sent to your email address. Please follow the link attached with the mail to verify your email.",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: "Times New Roman",
+                        fontWeight: FontWeight.w400),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: h * .02,
+                  ),
+                  State is AuthVerificationLoading
+                      ? const Text("Redirecting to Homepage...")
+                      : TextButton(
+                          onPressed: _goBack,
+                          child: SizedBox(
+                            width: w * .5,
+                            height: h * .05,
+                            child: const Center(
+                              child: ListTile(
+                                leading: Icon(
+                                  Icons.arrow_back,
+                                  color: Colors.blue,
+                                ),
+                                title: Text(
+                                  "Signup Page",
+                                  style: TextStyle(color: Colors.blue),
+                                ),
+                              ),
+                            ),
+                          )),
+                  State is AuthVerificationLoading
+                      ? const CupertinoActivityIndicator()
+                      : TextButton(
+                          onPressed: _resendMail,
+                          child: SizedBox(
+                            width: w * .5,
+                            height: h * .04,
+                            child: const ListTile(
+                              leading: Icon(
+                                Icons.refresh,
+                                color: Colors.green,
+                              ),
+                              title: Text(
+                                "Resend mail",
+                                style: TextStyle(color: Colors.green),
+                              ),
+                            ),
+                          )),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
