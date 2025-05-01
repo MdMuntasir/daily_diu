4a7afb4be89aea641769ea38eaad621dba643422
Muntasir <md.muntasir2700@gmail.com>
2025-05-01 08:33:46 +0600

    Adding search result feature

--- lib/features/result analysis/domain/repository/resultRepository.dart
+++ lib/features/result analysis/domain/repository/resultRepository.dart
@@ -2,7 +2,8 @@ import 'package:diu_student/core/resources/data_state.dart';
 import 'package:diu_student/features/result%20analysis/domain/entities/semesterResultEntity.dart';
 
 abstract interface class ResultRepo {
-  Future<DataState<List<List<SemesterResultEntity>>>> getResult();
+  Future<DataState<List<List<SemesterResultEntity>>>> getResult(String id);
 
-  Future<DataState<List<SemesterResultEntity>>> getSemesterResult();
+  Future<DataState<List<SemesterResultEntity>>> getSemesterResult(
+      String id, String semesterID);
 }
