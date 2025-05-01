997ac5f6e5af85b92cdabfa40063c99b6f444f6b
Muntasir <md.muntasir2700@gmail.com>
2025-05-01 06:54:02 +0600

    Adding search result feature

--- lib/features/result analysis/presentation/state/result_event.dart
+++ lib/features/result analysis/presentation/state/result_event.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/cupertino.dart';
 
 @immutable
-sealed class ResultEvent {}
+sealed class ResultEvent {
+  const ResultEvent();
+}
 
 class ResultInitialEvent extends ResultEvent {}
 
 class ResultCGPAShowActionEvent extends ResultEvent {}
 
 class ResultNavigateToNavBarEvent extends ResultEvent {}
+
+class SearchResultEvent extends ResultEvent {
+  final String studentId;
+
+  const SearchResultEvent(this.studentId);
+}
