43f29467c8dbed7809390ff0e186199b4a201f60
Muntasir <md.muntasir2700@gmail.com>
2025-05-01 08:33:12 +0600

    Adding search result feature

--- lib/features/result analysis/presentation/state/result_bloc.dart
+++ lib/features/result analysis/presentation/state/result_bloc.dart
@@ -7,6 +7,7 @@ import 'package:diu_student/features/result%20analysis/data/data%20sources/local
 import 'package:diu_student/features/result%20analysis/data/data%20sources/remote/api_data_source.dart';
 import 'package:diu_student/features/result%20analysis/data/repository/resultRepositoryImp.dart';
 import 'package:diu_student/features/result%20analysis/domain/usecase/result_usecase.dart';
+import 'package:diu_student/features/result%20analysis/domain/usecase/semester_result_usecase.dart';
 import 'package:diu_student/features/result%20analysis/presentation/state/result_event.dart';
 import 'package:diu_student/features/result%20analysis/presentation/state/result_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -28,13 +29,13 @@ class ResultBloc extends Bloc<ResultEvent, ResultState> {
       ResultInitialEvent event, Emitter<ResultState> emit) async {
     emit(ResultLoadingState());
     final dataState = await ResultUseCase(ResultRepositoryImpl(
-            RemoteResultImpl(user.studentID ?? "242-35-999"),
+            RemoteResultImpl(),
             RemoteSemestersImpl(),
             ConnectionCheckerImpl(
               InternetConnection(),
             ),
             ResultLocalDataSourceImpl(Hive.box("Results"))))
-        .call();
+        .call(para: user.studentID ?? "242-35-999");
     if (dataState is DataSuccess) {
       if (dataState.data!.isNotEmpty) {
         double cgpa = 0;
@@ -56,31 +57,23 @@ class ResultBloc extends Bloc<ResultEvent, ResultState> {
 
   FutureOr<void> searchResultEvent(
       SearchResultEvent event, Emitter<ResultState> emit) async {
-    emit(ResultLoadingState());
-    final dataState = await ResultUseCase(ResultRepositoryImpl(
-            RemoteResultImpl(event.studentId),
+    emit(SearchResultLoadingState());
+    final dataState = await SemesterResultUseCase(ResultRepositoryImpl(
+            RemoteResultImpl(),
             RemoteSemestersImpl(),
             ConnectionCheckerImpl(
               InternetConnection(),
             ),
             ResultLocalDataSourceImpl(Hive.box("Results"))))
-        .call();
+        .call(
+            para: SemesterResultParameter(
+      event.studentId,
+      event.semesterId,
+    ));
     if (dataState is DataSuccess) {
-      if (dataState.data!.isNotEmpty) {
-        double cgpa = 0;
-        for (var semester in dataState.data!) {
-          cgpa += semester[0].cgpa;
-        }
-        cgpa = double.parse((cgpa / dataState.data!.length).toStringAsFixed(2));
-        emit(ResultSuccessState(
-          results: dataState.data!,
-          cgpa: cgpa,
-        ));
-      } else {
-        emit(ResultEmptyState());
-      }
-    } else if (dataState is DataFailed) {
-      emit(ResultFailureState(dataState.error.toString()));
+      emit(SearchResultSuccessState(dataState.data!));
+    } else {
+      emit(SearchResultFailedState(dataState.error!));
     }
   }
 
