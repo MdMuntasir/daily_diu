cb26df9368b093e3c7ffdae5b49659d7edd35337
Muntasir <md.muntasir2700@gmail.com>
2025-05-01 08:33:37 +0600

    Adding search result feature

--- lib/features/result analysis/domain/usecase/result_usecase.dart
+++ lib/features/result analysis/domain/usecase/result_usecase.dart
@@ -4,13 +4,15 @@ import 'package:diu_student/core/usecases/usecase.dart';
 import '../entities/semesterResultEntity.dart';
 import '../repository/resultRepository.dart';
 
-class ResultUseCase implements UseCase<List<List<SemesterResultEntity>>, void> {
+class ResultUseCase
+    implements UseCase<List<List<SemesterResultEntity>>, String> {
   final ResultRepo _resultRepo;
 
   const ResultUseCase(this._resultRepo);
 
   @override
-  Future<DataState<List<List<SemesterResultEntity>>>> call({para}) async {
-    return await _resultRepo.getResult();
+  Future<DataState<List<List<SemesterResultEntity>>>> call(
+      {String? para}) async {
+    return await _resultRepo.getResult(para!);
   }
 }
