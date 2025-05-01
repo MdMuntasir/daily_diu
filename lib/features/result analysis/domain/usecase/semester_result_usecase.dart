ceb4f6e71c17764459bb7b879ad86292a67c6f32
Muntasir <md.muntasir2700@gmail.com>
2025-05-01 08:32:48 +0600

    Adding search result feature

--- lib/features/result analysis/domain/usecase/semester_result_usecase.dart
+++ lib/features/result analysis/domain/usecase/semester_result_usecase.dart
@@ -4,14 +4,25 @@ import 'package:diu_student/core/usecases/usecase.dart';
 import '../entities/semesterResultEntity.dart';
 import '../repository/resultRepository.dart';
 
-class SemesterSemesterResultUseCase
-    implements UseCase<List<SemesterResultEntity>, void> {
+class SemesterResultUseCase
+    implements UseCase<List<SemesterResultEntity>, SemesterResultParameter> {
   final ResultRepo _resultRepo;
 
-  const SemesterSemesterResultUseCase(this._resultRepo);
+  const SemesterResultUseCase(this._resultRepo);
 
   @override
-  Future<DataState<List<SemesterResultEntity>>> call({para}) async {
-    return await _resultRepo.getSemesterResult();
+  Future<DataState<List<SemesterResultEntity>>> call(
+      {SemesterResultParameter? para}) async {
+    return await _resultRepo.getSemesterResult(para?.id, para?.semesterID);
   }
+}
+
+class SemesterResultParameter {
+  final id;
+  final semesterID;
+
+  const SemesterResultParameter(
+    this.id,
+    this.semesterID,
+  );
 }
