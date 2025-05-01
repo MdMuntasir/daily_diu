5ebb6b5749b74dbb6aa1c87b22999a0f0b3bd018
Muntasir <md.muntasir2700@gmail.com>
2025-05-01 06:53:43 +0600

    Adding search result feature

--- lib/core/remote info/main_info_model.dart
+++ lib/core/remote info/main_info_model.dart
@@ -3,21 +3,26 @@ import 'package:equatable/equatable.dart';
 
 class MainInfoEntity extends Equatable {
   final String api;
+  final String resultApi;
 
-  const MainInfoEntity({
-    required this.api,
-  });
+  const MainInfoEntity({required this.api, required this.resultApi});
 
   @override
   List<Object?> get props => [api];
 }
 
 class MainInfoModel extends MainInfoEntity {
-  const MainInfoModel({required super.api});
+  const MainInfoModel({
+    required super.api,
+    required super.resultApi,
+  });
 
   factory MainInfoModel.fromSnapShot(
       DocumentSnapshot<Map<String, dynamic>> document) {
     final map = document.data()!;
-    return MainInfoModel(api: map["API"] ?? "");
+    return MainInfoModel(
+      api: map["API"] ?? "",
+      resultApi: map["resultApi"] ?? "",
+    );
   }
 }
