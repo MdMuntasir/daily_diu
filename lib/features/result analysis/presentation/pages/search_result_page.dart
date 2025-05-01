5d637dabdc8d1a7ee8360ff300f69ce6304a45ac
Muntasir <md.muntasir2700@gmail.com>
2025-05-01 06:48:16 +0600

    Adding search result feature

---
+++ lib/features/result analysis/presentation/pages/search_result_page.dart
@@ -0,0 +1,169 @@
+import 'package:diu_student/core/util/widgets/error_screen.dart';
+import 'package:diu_student/features/result%20analysis/presentation/state/result_bloc.dart';
+import 'package:diu_student/features/result%20analysis/presentation/state/result_event.dart';
+import 'package:diu_student/features/result%20analysis/presentation/state/result_state.dart';
+import 'package:diu_student/features/result%20analysis/presentation/widgets/all_semester_result.dart';
+import 'package:diu_student/features/result%20analysis/presentation/widgets/cgpa_bar.dart';
+import 'package:diu_student/features/result%20analysis/presentation/widgets/display_cgpa.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
+
+import '../../../navbar/presentation/pages/NavBar.dart';
+
+class SearchResultPage extends StatefulWidget {
+  const SearchResultPage({super.key});
+
+  @override
+  State<SearchResultPage> createState() => _SearchResultPageState();
+}
+
+class _SearchResultPageState extends State<SearchResultPage> {
+  final TextEditingController _searchController = TextEditingController();
+  bool cgpaShow = false;
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
+
+    return BlocConsumer(
+      bloc: context.read<ResultBloc>(),
+      listenWhen: (prev, current) => current is ResultActionState,
+      buildWhen: (prev, current) =>
+          current is! ResultActionState || current is ResultFailureState,
+      listener: (context, state) {
+        if (state is ResultNavigateToNavBarActionState) {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => const NavBar()));
+        }
+      },
+      builder: (context, state) {
+        switch (state.runtimeType) {
+          case ResultLoadingState:
+            return Center(
+              child: Lottie.asset(
+                "assets/lottie/Loading1.json",
+                height: h * .3,
+              ),
+            );
+
+          case ResultSuccessState:
+            final successState = state as ResultSuccessState;
+            final results = successState.results.reversed.toList();
+
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: h * .03,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: w * .05),
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: InputDecoration(
+                        hintText: 'Enter Student ID',
+                        prefixIcon: const Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.search),
+                          onPressed: () {
+                            if (_searchController.text.isNotEmpty) {
+                              context.read<ResultBloc>().add(
+                                  SearchResultEvent(_searchController.text));
+                            }
+                          },
+                        ),
+                      ),
+                      onSubmitted: (value) {
+                        if (value.isNotEmpty) {
+                          context
+                              .read<ResultBloc>()
+                              .add(SearchResultEvent(value));
+                        }
+                      },
+                    ),
+                  ),
+                  DisplayCgpa(
+                    showAvg: cgpaShow,
+                    results: results,
+                    cgpa: successState.cgpa,
+                  ),
+                  CgpaBar(
+                    cgpa: successState.cgpa,
+                    avgShow: cgpaShow,
+                    func: () {
+                      setState(() {
+                        cgpaShow = !cgpaShow;
+                      });
+                    },
+                  ),
+                  AllSemesterResult(
+                    height: h * .35,
+                    width: w,
+                    results: successState.results,
+                    mainContext: context,
+                  )
+                ],
+              ),
+            );
+
+          case ResultFailureState:
+            final failureState = state as ResultFailureState;
+            return ErrorScreen(
+              onPressed: () {
+                context.read<ResultBloc>().add(ResultInitialEvent());
+              },
+              message: failureState.errorMessage,
+            );
+
+          case ResultEmptyState:
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: h * .02,
+              children: [
+                Lottie.asset(
+                  "assets/lottie/DataError.json",
+                  width: w * .6,
+                  height: w * .6,
+                  fit: BoxFit.contain,
+                ),
+                SizedBox(
+                  width: w,
+                ),
+                SizedBox(
+                  width: w * .6,
+                  child: Center(
+                    child: Text(
+                      "No data found. Please enter a valid student ID.",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+
+          default:
+            return ErrorScreen(
+              onPressed: () {
+                context.read<ResultBloc>().add(ResultInitialEvent());
+              },
+            );
+        }
+      },
+    );
+  }
+}
