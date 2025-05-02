a8eb70a3257bd15a1c8e02856adc9d3618707549
Muntasir <md.muntasir2700@gmail.com>
2025-05-02 21:03:41 +0600

    Updating UI

--- lib/features/authentication/presentation/pages/login.dart
+++ lib/features/authentication/presentation/pages/login.dart
@@ -10,7 +10,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../home/presentation/pages/homePage.dart';
 import '../widgets/textStyle.dart';
 import 'email_varification_page.dart';
 
