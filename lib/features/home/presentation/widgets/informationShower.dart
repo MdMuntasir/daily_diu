94e01abd87d9e69ff97369835053a3548a622591
Muntasir <md.muntasir2700@gmail.com>
2025-05-03 16:22:14 +0600

    Updating UI

--- lib/features/home/presentation/widgets/informationShower.dart
+++ lib/features/home/presentation/widgets/informationShower.dart
@@ -21,20 +21,13 @@ class StudentInfoShow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double h = MediaQuery
-        .of(context)
-        .size
-        .height;
-    double w = MediaQuery
-        .of(context)
-        .size
-        .width;
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
     final theme = Theme.of(context).extension<InformationShowerTheme>()!;
 
     return Padding(
       padding: EdgeInsets.symmetric(vertical: h * .01, horizontal: w * .06),
       child: Container(
-        padding: EdgeInsets.symmetric(horizontal: w * .07, vertical: h * .02),
         width: w,
         height: h * .22,
         decoration: BoxDecoration(
@@ -47,41 +40,61 @@ class StudentInfoShow extends StatelessWidget {
                 color: theme.bgShadowColor,
               )
             ]),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        clipBehavior: Clip.antiAlias,
+        child: Stack(
+          fit: StackFit.expand,
           children: [
-            CustomText(
-              first: "Name",
-              second: Name,
-              color: theme.fgColor,
-              shadowColor: theme.fgShadowColor,
-              size: 15,
-              separate: true,
+            ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                theme.bgShapeColor,
+                BlendMode.srcATop,
+              ),
+              child: Image.asset(
+                "assets/images/leaf.png",
+                fit: BoxFit.cover,
+              ),
             ),
-            CustomText(
-              first: "ID",
-              second: ID,
-              color: theme.fgColor,
-              shadowColor: theme.fgShadowColor,
-              size: 15,
-              separate: true,
-            ),
-            CustomText(
-              first: "Department",
-              second: Department,
-              color: theme.fgColor,
-              shadowColor: theme.fgShadowColor,
-              size: 15,
-              separate: true,
-            ),
-            CustomText(
-              first: "Section",
-              second: "$Batch-$Section",
-              color: theme.fgColor,
-              shadowColor: theme.fgShadowColor,
-              size: 15,
-              separate: true,
+            Padding(
+              padding:
+                  EdgeInsets.symmetric(horizontal: w * .07, vertical: h * .02),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomText(
+                    first: "Name",
+                    second: Name,
+                    color: theme.fgColor,
+                    shadowColor: theme.fgShadowColor,
+                    size: 15,
+                    separate: true,
+                  ),
+                  CustomText(
+                    first: "ID",
+                    second: ID,
+                    color: theme.fgColor,
+                    shadowColor: theme.fgShadowColor,
+                    size: 15,
+                    separate: true,
+                  ),
+                  CustomText(
+                    first: "Department",
+                    second: Department,
+                    color: theme.fgColor,
+                    shadowColor: theme.fgShadowColor,
+                    size: 15,
+                    separate: true,
+                  ),
+                  CustomText(
+                    first: "Section",
+                    second: "$Batch-$Section",
+                    color: theme.fgColor,
+                    shadowColor: theme.fgShadowColor,
+                    size: 15,
+                    separate: true,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -95,7 +108,6 @@ class TeacherInfoShow extends StatelessWidget {
   final String Faculty;
   final String Department;
   final String TeacherInitial;
-  final Gradient grad;
 
   const TeacherInfoShow({
     super.key,
@@ -103,88 +115,84 @@ class TeacherInfoShow extends StatelessWidget {
     required this.Department,
     required this.Faculty,
     required this.TeacherInitial,
-    required this.grad,
   });
 
   @override
   Widget build(BuildContext context) {
-    double h = MediaQuery
-        .of(context)
-        .size
-        .height;
-    double w = MediaQuery
-        .of(context)
-        .size
-        .width;
-    bool horizontal = h > w;
-    double round = horizontal ? w * .15 : h * .25;
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
+    final theme = Theme.of(context).extension<InformationShowerTheme>()!;
 
-    return Container(
-      width: horizontal ? w : w * .8,
-      height: horizontal ? h * .4 : h * .6,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        gradient: grad,
-        boxShadow: const [
-          BoxShadow(spreadRadius: -10, blurRadius: 20, color: Colors.blue)
-        ],
-        borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(round),
-            bottomRight: Radius.circular(round)),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 35),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: h * .01, horizontal: w * .06),
+      child: Container(
+        width: w,
+        height: h * .22,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: theme.bgColor,
+            boxShadow: [
+              BoxShadow(
+                spreadRadius: -10,
+                blurRadius: 15,
+                color: theme.bgShadowColor,
+              )
+            ]),
+        clipBehavior: Clip.antiAlias,
+        child: Stack(
+          fit: StackFit.expand,
           children: [
-            SizedBox(
-              height: h * .08,
+            ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                theme.bgShapeColor,
+                BlendMode.srcATop,
+              ),
+              child: Image.asset(
+                "assets/images/leaf.png",
+                fit: BoxFit.cover,
+              ),
             ),
-            Text(
-              "Welcome " + Name,
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .displayLarge!
-                  .copyWith(
-                  fontSize: horizontal ? w * .085 : 40,
-                  color: Colors.teal.shade800,
-                  fontFamily: "Funky_Coffee"),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: h * .045,
-            ),
-            CustomText(
-              first: "Teacher Initial",
-              second: TeacherInitial,
-              color: Colors.teal.shade700,
-              shadowColor: Colors.black45,
-              separate: true,
-            ),
-            SizedBox(
-              height: h * .03,
-            ),
-            CustomText(
-              first: "Faculty",
-              second: Faculty,
-              color: Colors.teal.shade700,
-              shadowColor: Colors.black45,
-              separate: true,
-            ),
-            SizedBox(
-              height: h * .03,
-            ),
-            CustomText(
-              first: "Department",
-              second: Department
-                  .split("+")
-                  .length > 1
-                  ? "${Department.split("+")[0]}..."
-                  : Department,
-              color: Colors.teal.shade700,
-              shadowColor: Colors.black45,
-              separate: true,
+            Padding(
+              padding:
+                  EdgeInsets.symmetric(horizontal: w * .07, vertical: h * .02),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomText(
+                    first: "Name",
+                    second: Name,
+                    color: theme.fgColor,
+                    shadowColor: theme.fgShadowColor,
+                    size: 15,
+                    separate: true,
+                  ),
+                  CustomText(
+                    first: "Teacher Initial",
+                    second: TeacherInitial,
+                    color: theme.fgColor,
+                    shadowColor: theme.fgShadowColor,
+                    size: 15,
+                    separate: true,
+                  ),
+                  CustomText(
+                    first: "Faculty",
+                    second: Faculty,
+                    color: theme.fgColor,
+                    shadowColor: theme.fgShadowColor,
+                    size: 15,
+                    separate: true,
+                  ),
+                  CustomText(
+                    first: "Department",
+                    second: Department,
+                    color: theme.fgColor,
+                    shadowColor: theme.fgShadowColor,
+                    size: 15,
+                    separate: true,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -192,5 +200,3 @@ class TeacherInfoShow extends StatelessWidget {
     );
   }
 }
-
-
