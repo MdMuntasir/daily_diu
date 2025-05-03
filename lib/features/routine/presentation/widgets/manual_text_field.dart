d67da758d2a55ea3b3c70eb05ff8b0d25caa80a1
Muntasir <md.muntasir2700@gmail.com>
2025-05-03 16:23:10 +0600

    Updating UI

--- lib/features/routine/presentation/widgets/manual_text_field.dart
+++ lib/features/routine/presentation/widgets/manual_text_field.dart
@@ -3,57 +3,60 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
+import '../../../../config/theme/custom themes/routine_theme.dart';
+
 class ManualTextField extends StatelessWidget {
   final TextEditingController controller;
   final String title;
   final bool isDigit;
-  final int ? maxLen;
-  const ManualTextField({super.key,required this.controller, required this.title, this.isDigit = false, this.maxLen});
+  final int? maxLen;
+
+  const ManualTextField(
+      {super.key,
+      required this.controller,
+      required this.title,
+      this.isDigit = false,
+      this.maxLen});
 
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
+    final theme = Theme.of(context).extension<RoutineTheme>()!;
 
-    if(h<w){
-      h = w;
-      w = MediaQuery.of(context).size.height;
-    }
-
-
-    double space = w*.05;
+    double space = w * .05;
 
     return SizedBox(
-      width: w*.7,
+      width: w * .7,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.end,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
-              title + " : ",
+            title + " : ",
             style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).primaryColor,
-              fontSize: 16
-            ),
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColor,
+                fontSize: 16),
           ),
-          SizedBox(width: space,),
           SizedBox(
-            width: w*.3,
-            height: h*.05,
+            width: space,
+          ),
+          SizedBox(
+            width: w * .3,
+            height: h * .05,
             child: TextField(
               controller: controller,
               keyboardType: isDigit ? TextInputType.number : TextInputType.text,
-              inputFormatters: isDigit? [
-                FilteringTextInputFormatter.digitsOnly
-              ] : [FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z0-9\-]')),],
-              decoration: const InputDecoration(
-                hintStyle: TextStyle(fontSize: 18, color: Colors.black54),
-                focusColor: Colors.white,
-
-              ),
-              cursorColor: Colors.blueAccent.shade700,
-              style: TextStyle(color: Colors.lightBlueAccent.shade700, fontWeight: FontWeight.bold),
+              inputFormatters: isDigit
+                  ? [FilteringTextInputFormatter.digitsOnly]
+                  : [
+                      FilteringTextInputFormatter.allow(
+                          RegExp(r'[a-zA-Z0-9\-]')),
+                    ],
+              cursorColor: theme.deepColor,
+              style: TextStyle(
+                  color: theme.deepColor, fontWeight: FontWeight.bold),
               maxLength: maxLen,
             ),
           )
