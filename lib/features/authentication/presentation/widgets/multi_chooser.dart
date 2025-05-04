88b2def92146ba0eb3d74af27a6a6b215bb8e036
Muntasir <md.muntasir2700@gmail.com>
2025-05-04 10:54:44 +0600

    Updating UI

--- lib/features/authentication/presentation/widgets/multi_chooser.dart
+++ lib/features/authentication/presentation/widgets/multi_chooser.dart
@@ -1,3 +1,4 @@
+import 'package:diu_student/config/theme/custom%20themes/form_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -5,7 +6,12 @@ class MultiChooser extends StatefulWidget {
   final TextEditingController controller;
   final Map map;
   final String title;
-  const MultiChooser({super.key, required this.controller, required this.map, this.title = "None"});
+
+  const MultiChooser(
+      {super.key,
+      required this.controller,
+      required this.map,
+      this.title = "None"});
 
   @override
   State<MultiChooser> createState() => _MultiChooserState();
@@ -16,55 +22,56 @@ class _MultiChooserState extends State<MultiChooser> {
   String selected = "";
   TextEditingController deptController = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context).extension<FormTheme>()!;
 
-    if(selected == ""){
+    if (selected == "") {
       selected = widget.title;
     }
 
-
     return ElevatedButton(
-        onPressed: (){
+        onPressed: () {
           showDialog(
               context: context,
-              builder: (context)=>ChooseDepartment(
-                  map: widget.map,
-                  controller: deptController,
-                  func: (){setState(() {
-                    selected = deptController.text != ""? deptController.text : "Department";
-                    widget.controller.text = deptController.text;
-                  });},
-              ));
+              builder: (context) => ChooseDepartment(
+                    map: widget.map,
+                    controller: deptController,
+                    func: () {
+                      setState(() {
+                        selected = deptController.text != ""
+                            ? deptController.text
+                            : "Department";
+                        widget.controller.text = deptController.text;
+                      });
+                    },
+                  ));
         },
         style: ButtonStyle(
-            backgroundColor: WidgetStatePropertyAll(Colors.white),
-            foregroundColor: WidgetStatePropertyAll(Colors.blue.shade500),
+            backgroundColor: WidgetStatePropertyAll(theme.chooserBgColor),
+            foregroundColor: WidgetStatePropertyAll(theme.chooserFgColor),
             elevation: WidgetStatePropertyAll(1.5),
-            shadowColor: WidgetStatePropertyAll(Colors.teal),
-            shape: WidgetStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))))
-        ),
+            shadowColor: WidgetStatePropertyAll(theme.deepColor),
+            shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10))))),
         child: Text(
-          selected == "Department"? selected : selected.split('-')[0] + '...',
-          style: TextStyle(fontSize: 18),)
-    );
+          selected == "Department" ? selected : selected.split('-')[0] + '...',
+          style: TextStyle(fontSize: 18),
+        ));
   }
 }
-
-
-
-
-
-
-
-
 
 class ChooseDepartment extends StatefulWidget {
   final VoidCallback func;
   final TextEditingController controller;
   final Map map;
-  const ChooseDepartment({super.key, required this.map, required this.controller, required this.func, });
+
+  const ChooseDepartment({
+    super.key,
+    required this.map,
+    required this.controller,
+    required this.func,
+  });
 
   @override
   State<ChooseDepartment> createState() => _ChooseDepartmentState();
@@ -72,71 +79,66 @@ class ChooseDepartment extends StatefulWidget {
 
 class _ChooseDepartmentState extends State<ChooseDepartment> {
   bool isClicked = false;
-  Map<String,bool> boolValues = {};
-
+  Map<String, bool> boolValues = {};
 
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
+    final theme = Theme.of(context).extension<FormTheme>()!;
 
-
-    if(!isClicked){
-      widget.map.forEach((short, long){
-        boolValues[short] = false;});
+    if (!isClicked) {
+      widget.map.forEach((short, long) {
+        boolValues[short] = false;
+      });
     }
-
 
     List<Widget> options = [];
 
     List lst = [];
 
-
-
-    widget.map.forEach((short, long){
-
-      lst.add([short,long]);
+    widget.map.forEach((short, long) {
+      lst.add([short, long]);
       options.add(CheckboxListTile(
           value: boolValues[short],
           checkboxShape: CircleBorder(),
           controlAffinity: ListTileControlAffinity.leading,
-          checkColor: Colors.white,
-          activeColor: Colors.blue,
-
+          checkColor: theme.chooserFgColor,
+          activeColor: theme.chooserBgColor,
           title: Text(
             long,
             style: TextStyle(
-                color: Colors.blue.shade500, fontWeight: FontWeight.bold),
+                color: theme.chooserBgColor, fontWeight: FontWeight.bold),
           ),
-          onChanged: (newVal){
+          onChanged: (newVal) {
             boolValues[short] = !boolValues[short]!;
             setState(() {
               isClicked = true;
             });
           }));
-
     });
 
-
-
     return AlertDialog(
-      content : SizedBox(
-          width: w*.85,
-          height: widget.map.length * h *.06 + h*.05,
+      content: SizedBox(
+          width: w * .85,
+          height: widget.map.length * h * .06 + h * .05,
           child: SingleChildScrollView(
-            child: Column(children: options,),
-          )
-      ),
+            child: Column(
+              children: options,
+            ),
+          )),
       actions: [
         ElevatedButton(
-            onPressed: (){
+            onPressed: () {
               String temp = "";
-              boolValues.forEach((dept,checked){
-                if(checked){
+              boolValues.forEach((dept, checked) {
+                if (checked) {
                   temp += dept + "-";
                 }
               });
-              if(temp.isNotEmpty){temp = temp.substring(0,temp.length-1);}
+              if (temp.isNotEmpty) {
+                temp = temp.substring(0, temp.length - 1);
+              }
               widget.controller.text = temp;
               widget.func();
               Navigator.pop(context);
@@ -146,4 +148,3 @@ class _ChooseDepartmentState extends State<ChooseDepartment> {
     );
   }
 }
-
