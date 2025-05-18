bccfafbd697c08070982ce2fabee1394a2fa661d
Muntasir <md.muntasir2700@gmail.com>
2025-05-18 07:39:02 +0600

    Updating UI

--- lib/core/util/widgets/show_message.dart
+++ lib/core/util/widgets/show_message.dart
@@ -1,36 +1,34 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ShowAlertMessage extends StatelessWidget {
   final bool hasSucceed;
   final String text;
-  const ShowAlertMessage({super.key, required this.text, this.hasSucceed = false, });
+
+  const ShowAlertMessage({
+    super.key,
+    required this.text,
+    this.hasSucceed = false,
+  });
 
   @override
   Widget build(BuildContext context) {
-
-
     return AlertDialog(
-
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          hasSucceed ?  Text("Succeed!") : Text("Alert!"),
+          hasSucceed ? const Text("Succeed!") : const Text("Alert!"),
           IconButton(
-              onPressed: (){
+              onPressed: () {
                 Navigator.pop(context);
               },
-              icon: Icon(FontAwesomeIcons.xmark))
+              icon: const Icon(FontAwesomeIcons.xmark))
         ],
       ),
-
       content: Text(
         text,
-        style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w400
-        ) , ),
+        style: const TextStyle(fontWeight: FontWeight.w400),
+      ),
     );
   }
 }
